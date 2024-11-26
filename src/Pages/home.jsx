407b67a225d8a48eba257ab7daccf335cf87e618
Braydon Lowe <braydonslowe17@gmail.com>
2024-11-25 21:47:41 -0700

    Header replaced in home.jsx

--- src/Pages/home.jsx
+++ src/Pages/home.jsx
@@ -1,31 +1,10 @@
 import React from "react";
+import { Header } from "./Components/Header"
 
 export const Home = () => {
   return (
     <div className="wrapper">
-      <header>
-        <h1>
-          <img
-            src="../Pictures/CozyMae_FullLogo.png"
-            alt="CozyMae Floral"
-            width="200"
-            height="84"
-          />
-          <span className="image-alt">CozyMae Floral</span>
-        </h1>
-        <div className="auth-button">
-          <a href="/login">Login/Register</a>
-        </div>
-        <nav>
-          <ul>
-            <li><a href="/dashboard">Dashboard</a></li>
-            <li><a href="/calendar">Calendar</a></li>
-            <li><a href="/appointments">Appointments</a></li>
-            <li><a href="/contracts">Contracts</a></li>
-            <li><a href="/profile">Profile</a></li>
-          </ul>
-        </nav>
-      </header>
+        <Header />
       <main>
         <section>
           <h2 className="header-text">Welcome to CozyMae Floral!</h2>
