cc6429f90dcf96aebf1dc6c44b4d71bf47012f91
Braydon Lowe <braydonslowe17@gmail.com>
2024-11-26 20:03:52 -0700

    init(dashboard.jsx)

---
+++ src/Pages/Dashboard/dashboard.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Header } from "../Components/Header"
+import { Footer } from "../Components/Header"
+
+
+export const Dashboard = () => {
+    return (
+        <div className="wrapper">
+            <Header />
+
+
+
+            <Footer />
+        </div>
+    )
+}
