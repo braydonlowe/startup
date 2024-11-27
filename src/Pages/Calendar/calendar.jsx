2b8e7f3c2f24e8debf3632332e602d86e7403305
Braydon Lowe <braydonslowe17@gmail.com>
2024-11-26 21:40:50 -0700

    calendar.css first draft

--- src/Pages/Calendar/calendar.jsx
+++ src/Pages/Calendar/calendar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Header } from "../Components/Header/Header"
 import { Footer } from "../Components/Footer/Footer";
+import "./calendar.css"
 
 
 const Calendar = () => {
